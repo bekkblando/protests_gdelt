702402f7a3ccd89083859136470200c4c99a24b1
Bekk Blando <bekkblando@gmail.com>
2017-10-03 14:16:44 -0400

    exploring gdelt data

---
+++ exploring_gdelt_data.R
@@ -0,0 +1,77 @@
+rm(list = ls())
+
+install.packages("httr")
+install.packages("bigrquery")
+library("bigrquery")
+library(DBI)
+library(RSQLite)
+
+db_name <- file.path('data','events.sqlite')
+
+getwd()
+
+# Use your project ID here
+project <- "datascienceprotest" # put your project ID here
+
+# Getting some relevent data
+# Modify columns for different visualization
+sql <- "SELECT Actor1Name, Actor2Name, EventCode, AvgTone, GoldsteinScale, IsRootEvent, QuadClass, NumMentions, NumSources, Actor1Geo_Lat, Actor1Geo_Long, Actor2Geo_Lat, Actor2Geo_Long, FractionDate FROM [gdelt-bq:full.events] WHERE EventRootCode='14' and Year=2017"
+
+
+# Execute the query and store the result NOTICE max_pages is set to Inf 
+# Try not to run this too much or remove max_pages to get the first page
+protests <- query_exec(sql, project = project, max_pages = Inf)
+
+# NOTICE: Change the file path to yours
+db_name <- file.path('/Users/BekkBlando/Documents/github/clemson/data_science/protests/data','events.sqlite')
+db_conn <- dbConnect(SQLite(), db_name)
+
+
+if (!dbExistsTable(db_conn, 'events')){
+  dbSendQuery(conn = db_conn,
+              'CREATE TABLE Events (Actor1Name TEXT, Actor2Name TEXT, EventCode INTEGER, AvgTone INTEGER, GoldsteinScale INTEGER, IsRootEvent INTEGER, QuadClass INTEGER, NumMentions INTEGER, NumSources INTEGER, Actor1Geo_Lat FLOAT, Actor1Geo_Long FLOAT, Actor2Geo_Lat FLOAT, Actor2Geo_Long FLOAT, FractionDate FLOAT)')
+}
+
+# Check it out yo
+dbListTables(db_conn)
+dbListFields(db_conn, 'Events') 
+
+# Read our protests data in
+dbWriteTable(db_conn, 'Events', protests, append=TRUE)
+
+dbDisconnect(db_conn)
+# Now that it's loaded in lets free some mem
+rm(list = ls())
+
+db_name <- file.path('/Users/BekkBlando/Documents/github/clemson/data_science/protests/data','events.sqlite')
+db_conn <- dbConnect(SQLite(), db_name)
+
+# Checkin out the data
+local_data <- dbReadTable(db_conn, 'Events')
+
+
+# Do Stuff yo
+
+attach(local_data)
+
+summary(local_data)
+
+
+counts <- aggregate(local_data[4], list(EventCode), mean)
+c(counts["Group.1"])[1]
+c(counts$AvgTone)[2]
+
+barplot(c(counts$AvgTone), names.arg=c(counts$Group.1))
+
+nrow(local_data)
+
+# Exploring The data
+length(which(EventCode == 145 & (Actor1Name=="UNITED STATES" | Actor2Name=="UNITED STATES")))/length(which(Actor1Name=="UNITED STATES" | Actor2Name=="UNITED STATES")) * 100
+
+
+nrow(local_data[ which(local_data$Actor1Name == "UNITED STATES" && local_data$EventCode == 145),])
+
+length(which(EventCode == 145))/nrow(local_data) * 100
+
+
+
