82f96a6cfc6589957cff8d9a6967c6cea98310c6
Bekk Blando <bekkblando@gmail.com>
2017-11-16 16:13:13 -0500

    install packages if needed for heroku

--- run.R
+++ run.R
@@ -1,8 +1,17 @@
 library(shiny)
-install.packages("leaflet")
 library(leaflet)
-# install.packages("plyr")
 library(plyr)
+
+my_packages = c("shiny", "leaflet", "plyr")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
+
 
 port <- Sys.getenv('PORT')
 
@@ -46,14 +55,14 @@ server <- function(input, output, session) {
         options = providerTileOptions(noWrap = TRUE)
       ) %>%
 
-      addAwesomeMarkers(root_events$ActionGeo_Long, root_events$ActionGeo_Lat, layerId=root_events$GLOBALEVENTID, 
+      addAwesomeMarkers(root_events$ActionGeo_Long, root_events$ActionGeo_Lat, layerId=root_events$GLOBALEVENTID,
                         icon=ricons, popup = root_events$Actor1Name, group = "root_events")
     })
-  
+
   # Show popup on click
   observeEvent(input$map_marker_click, {
     click <- input$map_marker_click
-    
+
     # Show non-root events based on the root event
     sequence = root_events[which(root_events$GLOBALEVENTID == click$id),]$sequence
     # = non_root[which(non_root$GLOBALEVENTID == sequence)]
@@ -64,19 +73,19 @@ server <- function(input, output, session) {
       print(paste0("No Coords Yo"))
       return("No Coordinates")
     }
-    
+
     print(paste0(typeof(non_root_seq)))
-    
-    
+
+
     # text<-paste("Lattitude ", click$id, "Longtitude ", click$lng)
     proxy <- leafletProxy("map")
 
     # Hide all the root events
     clearGroup(proxy, "root_events")
     clearGroup(proxy, "sequence")
-    
+
     # Show the sequence
-    proxy %>% addAwesomeMarkers(non_root_seq$ActionGeo_Long, non_root_seq$ActionGeo_Lat, layerId=non_root_seq$GLOBALEVENTID, 
+    proxy %>% addAwesomeMarkers(non_root_seq$ActionGeo_Long, non_root_seq$ActionGeo_Lat, layerId=non_root_seq$GLOBALEVENTID,
                           icon=nicons, popup = non_root_seq$Actor1Name, group = "sequence")
     output$table <- renderDataTable(data.frame(non_root_seq))
   })
