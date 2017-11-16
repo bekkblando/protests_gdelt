3423626e1b0e6fc5f2fcdf299400bc739c1dabc9
ssweetm <32559527+ssweetm@users.noreply.github.com>
2017-11-16 13:36:11 -0500

    Add files via upload

--- pvp4.R
+++ pvp4.R
@@ -11,7 +11,7 @@ library(leaflet)
 #PROJECT ID
 pvp1 <- "pvp1-183719"
 
-#select predictors for protest events in between Jan-Mar 2016
+#select predictors for protest events in between Jan-Feb 2016
 #Note: event root code 14 = CAMEO Protest
 sql_14_2016 <- "SELECT Actor1Name, Actor2Name, EventCode, AvgTone, 
       GoldsteinScale, IsRootEvent, QuadClass, NumMentions, NumSources, 
@@ -42,18 +42,13 @@ dbGetQuery(conn = db_conn,
 
 }
 
-#lists names of tables accessed by db_conn
-#dbListTables(db_conn)
-
-#lists field names of remote table accessed by db_conn
-#dbListFields(db_conn, 'Events')
-
 #copy data frames to database tables
-#dbWriteTable(connection, table name, data)
 dbWriteTable(db_conn, 'Events', protests, append = TRUE)
 
 #read in table to workspace
 events <- dbReadTable(db_conn, 'Events')
+
+dbDisconnect(db_conn)
 
 #define objects for visualization
 event_tone = data.frame(EventCode = EventCode, AvgTone = AvgTone)
@@ -76,11 +71,41 @@ create_sequence <- function(row) {
 root_event$sequence = apply(root_event, 1, function(row) { 
   create_sequence(row) })
 
+#____________________________________GRAPHS______________________________________
+
+symbols(events$NumMentions, events$AvgTone, circles=events$EventCode, inches=0.35, 
+        fg="white", bg="blue", xlab = "Number of Mentions", ylab = "Average Tone") 
+
+symbols(events$GoldsteinScale, events$NumMentions, circles = events$IsRootEvent,
+        inches=0.15, fg="white", bg="red", xlab = "Goldstein Scale", 
+        ylab = "Number of Mentions")
+
+this_base <- "events"
+
+my_data <- data.frame(
+  event_code = factor(events$EventCode), mentions = data(events$NumMentions)
+)
+p <- ggplot2::ggplot(my_data, aes(x = event_code, y = mentions)) +
+  geom_bar(stat = "identity", fill = "orange", colours(distinct = TRUE), 
+  width = .02) + scale_y_continuous(breaks = sequence(events$NumMentions)) +
+  labs(x = "Event Code", y = "Number of Mentions") +
+  ggtitle("Number of Mentions per Event Code") +
+  theme_bw() +
+  theme(panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_line(colour = "grey50"),
+        plot.title = element_text(size = rel(1.5), face = "bold", 
+                                  vjust = 1.5), axis.title = element_text(face = "bold"))
+p
+
+
+
 #________________________RShiny Visualization__________________________________
+jitter(root_event$ActionGeo_Lat, factor = 0.0001)
 
 ui <- fluidPage(
-  leafletOutput("map")
-)
+  fluidRow(column(12, leafletOutput("map"))),
+  fluidRow(column(12, dataTableOutput('table')))
+  )
 
 ricons <- awesomeIcons(
   icon = 'ios-close',
@@ -99,7 +124,7 @@ event_map <- function(input, output, session) {
   output$map <- renderLeaflet({
     leaflet() %>%
     addProviderTiles(providers$Stamen.TonerLite,
-              options = providerTileOptions(noWrap = FALSE)) %>%
+              options = providerTileOptions(noWrap = TRUE)) %>%
     
     addAwesomeMarkers(root_event$Actor1Geo_Long, 
               root_event$Actor1Geo_Lat, layerId=root_event$GLOBALEVENTID, 
@@ -111,13 +136,27 @@ event_map <- function(input, output, session) {
     click <- input$map_marker_click
     
   # Show non-root events based on the root event
-  print(paste0(data.frame(root_event[which(root_event$GLOBALEVENTID == 
-    click$id),][17])$Actor1Geo_Long))
-    
-    text<-paste("Lattitude ", click$id, "Longtitude ", click$lng)
-    proxy <- leafletProxy("map")
-    proxy %>% clearPopups() %>%
-    addPopups(click$lng, click$lat, text)
+  sequence = root_events[which(root_event$GLOBALEVENTID == click$id),]$sequence
+  non_root_event = non_root_event[which(non_root_event$GLOBALEVENTID %in% sequence),]
+  
+  if(identical(non_root_event$ActionGeo_Long, numeric(0)) || length(sequence) == 0) {
+    print(paste0(sequence))
+    print(paste0("No Coordinates"))
+    return("No Coordinates")
+  }
+  
+  proxy <- leafletProxy("map")
+  
+  #hide all root events
+  clearGroup(proxy, "root_events")
+  clearGroup(proxy, "sequence")
+  
+  #show sequence
+  proxy %>% addAwesomeMarkers(non_root_event$ActionGeo_Long, non_root_event$ActionGeo_Lat,
+            layerId = non_root_event$GLOBALEVENTID, icons = bicons, 
+            popup = non_root_event$Actor1Name, group = "sequence")
+  
+  output$table <- renderDataTable(data.frame(non_root_seq))
   })
 }
 
