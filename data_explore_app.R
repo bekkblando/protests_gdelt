7ae2adba750497ad63a72a8a93b0bf34d4aa8702
Bekk Blando <bekkblando@gmail.com>
2017-11-16 15:48:01 -0500

    run.R for heroku

---
+++ data_explore_app.R
@@ -0,0 +1,103 @@
+library(shiny)
+library(leaflet)
+# install.packages("plyr")
+library(plyr)
+
+get_violent_protest <- function(year, month){
+  sql <- paste0("SELECT GLOBALEVENTID,ActionGeo_Lat, ActionGeo_Long, Actor1Name, Actor2Name, EventCode, EventRootCode, AvgTone, GoldsteinScale, IsRootEvent, QuadClass, NumMentions, NumSources, Actor1Geo_Lat, Actor1Geo_Long, Actor2Geo_Lat, Actor2Geo_Long, FractionDate FROM [gdelt-bq:full.events] WHERE EventCode='141' and MonthYear=", year, paste0(formatC(as.integer(month), width=2, flag="0")))
+  return(query_exec(sql, project = project, max_pages = Inf))
+}
+
+
+# Jitter the values yo
+jitter(root_events$ActionGeo_Long, factor = 0.0001)
+jitter(root_events$ActionGeo_Lat, factor = 0.0001)
+
+ui <- fluidPage(
+  # Year Picker
+  fluidRow(
+    column(12, selectInput("year", "Year", choices = c(" ", seq(2000,2017)))),
+    column(12, selectInput("month", "Month", choices = c(" ", seq(1,12)))),
+    column(12, actionButton("date_submit", "Submit"))
+  ),
+  fluidRow(
+    column(12, leafletOutput("map"))
+  ),
+  fluidRow(
+    column(12, dataTableOutput('table'))
+  )
+)
+
+ricons <- awesomeIcons(
+  icon = 'ios-close',
+  iconColor = 'black',
+  library = 'ion',
+  markerColor = "red"
+)
+nicons <- awesomeIcons(
+  icon = 'ios-close',
+  iconColor = 'black',
+  library = 'ion',
+  markerColor = "blue"
+)
+
+
+server <- function(input, output, session) {
+  
+  # Render Year Picker
+    # When year is selected, do google query for root events within the year
+  # Render this map once the google query has been selected
+  observeEvent(input$date_submit, {
+    # Render Loading Icon
+    click <- input$date_submit
+    
+    # Violent Protest Query
+    violent_protest <- get_violent_protest(input$year, input$month)
+    output$map <- renderLeaflet({
+      leaflet() %>%
+        addProviderTiles(providers$Stamen.TonerLite,
+                         options = providerTileOptions(noWrap = TRUE)
+        ) %>%
+        
+        addAwesomeMarkers(violent_protest$ActionGeo_Long, violent_protest$ActionGeo_Lat, layerId=violent_protest$GLOBALEVENTID, 
+                          icon=ricons, popup = violent_protest$Actor1Name, group = "root_events")
+    })
+  })
+  
+  # Show popup on click
+  observeEvent(input$map_marker_click, {
+    click <- input$map_marker_click
+    
+    # Show non-root events based on the violent protest
+    sequence = root_events[which(root_events$GLOBALEVENTID == click$id),]$sequence
+    # = non_root[which(non_root$GLOBALEVENTID == sequence)]
+    non_root_seq = non_root[which(non_root$GLOBALEVENTID %in% sequence),]
+    
+    if(identical(non_root_seq$ActionGeo_Long, numeric(0)) || length(sequence) == 0){
+      print(paste0(sequence))
+      print(paste0("No Coords Yo"))
+      return("No Coordinates")
+    }
+    
+    print(paste0(typeof(non_root_seq)))
+    
+    
+    # text<-paste("Lattitude ", click$id, "Longtitude ", click$lng)
+    proxy <- leafletProxy("map")
+    
+    # Hide all the root events
+    clearGroup(proxy, "root_events")
+    clearGroup(proxy, "sequence")
+    
+    # Show the sequence
+    # Mark the root events as a different color
+    proxy %>% addAwesomeMarkers(non_root_seq$ActionGeo_Long, non_root_seq$ActionGeo_Lat, layerId=non_root_seq$GLOBALEVENTID, 
+                                icon=ricons, popup = non_root_seq$Actor1Name, group = "sequence")
+    proxy %>% addAwesomeMarkers(non_root_seq$ActionGeo_Long, non_root_seq$ActionGeo_Lat, layerId=non_root_seq$GLOBALEVENTID, 
+                                icon=nicons, popup = non_root_seq$Actor1Name, group = "sequence")
+    # Render more notes if a violent protest exist within this
+    output$table <- renderDataTable(data.frame(non_root_seq))
+  })
+}
+
+shinyApp(ui, server)
--- pvp4.R
+++ pvp4.R
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #remove objects from workspace
 rm(list=ls())
 
@@ -161,3 +162,128 @@ event_map <- function(input, output, session) {
 }
 
 shinyApp(ui, event_map)
+=======
+#remove objects from workspace
+rm(list=ls())
+
+#load libraries
+library("bigrquery")
+library(DBI)
+library(RSQLite)
+library(shiny)
+library(leaflet)
+
+#PROJECT ID
+pvp1 <- "datascienceprotest"
+
+#select predictors for protest events in between Jan-Mar 2016
+#Note: event root code 14 = CAMEO Protest
+sql_14_2016 <- "SELECT Actor1Name, Actor2Name, EventCode, AvgTone, 
+      GoldsteinScale, IsRootEvent, QuadClass, NumMentions, NumSources, 
+      Actor1Geo_Lat, Actor1Geo_Long, Actor2Geo_Lat, Actor2Geo_Long, 
+      FractionDate FROM [gdelt-bq:full.events] WHERE EventRootCode='14' 
+      and MonthYear > 201601 and MonthYear < 201604"
+
+
+#execute bigQuery query and store result in protests list
+#query_exec(query string, project name, use_legacy_sql, max_pages)
+protests <- query_exec(sql_14_2016, project = pvp1, useLegacySql = FALSE, 
+                       max_pages = Inf)
+
+
+#construct file path to store
+db_name <- file.path('/Users/BekkBlando/Documents/github/clemson/data_science/protests/data/', 'events.sqlite')
+db_conn <- dbConnect(SQLite(), db_name)
+
+#getQuery (calls dbSendQuery/dbFetch/dbClearand create Events table
+if(!dbExistsTable(db_conn, 'Events')) {
+dbGetQuery(conn = db_conn, 
+            'CREATE TABLE Events(Actor1Name STRING, Actor2Name STRING, 
+            EventCode STRING, AvgTone FLOAT, GoldsteinScale FLOAT, 
+            IsRootEvent INTEGER, QuadClass INTEGER, NumMentions INTEGER, 
+            NumSources INTEGER, Actor1Geo_Lat FLOAT, Actor1Geo_Long FLOAT, 
+            Actor2Geo_Lat FLOAT, Actor2Geo_Long FLOAT, FractionDate FLOAT)')
+
+}
+
+#lists names of tables accessed by db_conn
+#dbListTables(db_conn)
+
+#lists field names of remote table accessed by db_conn
+#dbListFields(db_conn, 'Events')
+
+#copy data frames to database tables
+#dbWriteTable(connection, table name, data)
+dbWriteTable(db_conn, 'Events', protests, append = TRUE)
+
+#read in table to workspace
+events <- dbReadTable(db_conn, 'Events')
+
+#define objects for visualization
+event_tone = data.frame(EventCode = EventCode, AvgTone = AvgTone)
+events$EventCode <- as.factor(events$EventCode)
+sampled_data = events[sample(nrow(events), 500),]
+root_event = data.frame(sampled_data[which(sampled_data$IsRootEvent == TRUE),])
+non_root_event = data.frame(sampled_data[which(sampled_data$IsRootEvent == FALSE),])
+
+root_event$sequence <- NA
+
+create_sequence <- function(row) { 
+  event_query = non_root_event[which(non_root_event$Actor1Name == 
+                row["Actor1Name"], non_root_event$Actor2Name == 
+                row["Actor2Name"]),]
+  if(!identical(event_query, character(0)) && length(event_query) != 0) {
+    return(event_query)
+  }
+}
+
+root_event$sequence = apply(root_event, 1, function(row) { 
+  create_sequence(row) })
+
+#________________________RShiny Visualization__________________________________
+
+ui <- fluidPage(
+  leafletOutput("map")
+)
+
+ricons <- awesomeIcons(
+  icon = 'ios-close',
+  iconColor = 'black',
+  library = 'ion',
+  markerColor = "red"
+)
+bicons <- awesomeIcons(
+  icon = 'ios-close',
+  iconColor = 'black',
+  library = 'ion',
+  markerColor = "blue"
+)
+
+event_map <- function(input, output, session) {
+  output$map <- renderLeaflet({
+    leaflet() %>%
+    addProviderTiles(providers$Stamen.TonerLite,
+              options = providerTileOptions(noWrap = FALSE)) %>%
+    
+    addAwesomeMarkers(root_event$Actor1Geo_Long, 
+              root_event$Actor1Geo_Lat, layerId=root_event$GLOBALEVENTID, 
+              icon = ricons, popup = root_event$Actor1Name, group = "root")
+  })
+  
+  #Show popup on click
+  observeEvent(input$map_marker_click, {
+    click <- input$map_marker_click
+    
+  # Show non-root events based on the root event
+  print(paste0(data.frame(root_event[which(root_event$GLOBALEVENTID == 
+    click$id),][17])$Actor1Geo_Long))
+    
+    text<-paste("Lattitude ", click$id, "Longtitude ", click$lng)
+    proxy <- leafletProxy("map")
+    proxy %>% clearPopups() %>%
+    addPopups(click$lng, click$lat, text)
+  })
+}
+
+shinyApp(ui, event_map)
+>>>>>>> Stashed changes
