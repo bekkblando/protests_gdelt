e19a3acc3a3f1affff3008ab6ded4cb34edc73c1
Bekk Blando <bekkblando@gmail.com>
2017-11-26 17:39:12 -0500

    TODO's for sequences

--- scripts/sequences.R
+++ scripts/sequences.R
@@ -13,13 +13,23 @@ get_sequence <- function(violent_protest){
   sql <-paste0("SELECT GLOBALEVENTID,ActionGeo_Lat, ActionGeo_Long, Actor1Name, Actor2Name, EventCode, EventRootCode, AvgTone, GoldsteinScale, IsRootEvent, QuadClass, NumMentions, NumSources, Actor1Geo_Lat, Actor1Geo_Long, Actor2Geo_Lat, Actor2Geo_Long, FractionDate FROM [gdelt-bq:full.events] WHERE EventRootCode in ('10','11','12','13', '14') and FractionDate <=", higher_date ," and FractionDate >=", lower_date)
   if(!is.na(violent_actor1)){
     sql <- paste0(sql, " and Actor1Name='", violent_actor1, "'")
+  }else{
+    sql <- paste0(sql, " and Actor1Name is NULL")
   }
   if(!is.na(violent_actor2)){
     sql <- paste0(sql, " and Actor2Name='", violent_actor2, "'")
+  }else{
+    sql <- paste0(sql, " and Actor2Name is NULL")
   }
   print(sql)
   
-  return(query_exec(sql, project = project, max_pages = Inf))
+  
+  # TODO
+  # Average tone maximum for occurances - violent protests
+  # Above Average NumMentions for each event code
+  # Summary Statistics - Average Goldstien, AvgTone, NumMentions per EventCode 
+  
+  return(sequence)
 }
 
 
